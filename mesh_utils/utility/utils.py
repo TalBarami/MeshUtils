4cb3362baacf2578b0b09bff5fec582d97ffec59
TalB <talb@playtika.com>
2022-12-12 16:55:36 +0200

    - farthest point sampling

--- mesh_utils/utility/utils.py
+++ mesh_utils/utility/utils.py
@@ -3,6 +3,8 @@ import pickle
 from os import path as osp
 from pathlib import Path
 
+import numpy as np
+
 
 def init_logger(log_name, log_path=None):
     logger = logging.getLogger(log_name)
@@ -39,3 +41,26 @@ def read_pkl(file):
     except Exception as e:
         print(f'Error while reading {file}: {e}')
         raise e
+
+def farthest_point_sample(point, npoint):
+    """
+    Input:
+        xyz: points- points and data, [N, D]
+        npoint: number of points to samples
+    Return:
+        centroids: sampled pointcloud index, [npoint, D]
+    """
+    N, D = point.shape
+    xyz = point[:, :3]
+    centroids = np.zeros((npoint,))
+    distance = np.ones((N,)) * 1e10
+    farthest = np.random.randint(0, N)
+    for i in range(npoint):
+        centroids[i] = farthest
+        centroid = xyz[farthest, :]
+        dist = np.sum((xyz - centroid) ** 2, -1)
+        mask = dist < distance
+        distance[mask] = dist[mask]
+        farthest = np.argmax(distance, -1)
+    point = point[centroids.astype(np.int32)]
+    return point, centroids.astype(np.int32)
